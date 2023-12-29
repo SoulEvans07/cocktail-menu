50ce8e5a709b66a1d40c2c9606ea1bf978d37414
Adam Szi <szi.adam@simonyi.bme.hu>
2023-12-29 23:39:13 +0100

    render recipe

--- src/screens/collection-page/details.tsx
+++ src/screens/collection-page/details.tsx
@@ -1,25 +1,28 @@
 import { useMemo } from 'react';
-import { DrawerHeader, DrawerTitle } from '~/components/layout/drawer';
-import { SheetHeader, SheetTitle } from '~/components/layout/sheet';
-import { Cocktail } from '~/model/types/mvp';
+
+import type { PopulatedCocktail } from '~/model/types/populated';
+import { cn } from '~/utils/cn';
+import { Button } from '~/components/controls/button';
+import { DrawerClose, DrawerHeader, DrawerTitle } from '~/components/layout/drawer';
+import { SheetClose, SheetHeader, SheetTitle } from '~/components/layout/sheet';
 
 type CocktailDetailsProps = {
-  cocktail: Cocktail;
+  cocktail: PopulatedCocktail;
   isMobile?: boolean;
 };
 export function CocktailDetails(props: CocktailDetailsProps) {
   const { cocktail, isMobile } = props;
 
-  const { Header, Title } = useMemo(() => {
-    if (isMobile) return { Header: DrawerHeader, Title: DrawerTitle };
-    return { Header: SheetHeader, Title: SheetTitle };
+  const { Header, Title, Close } = useMemo(() => {
+    if (isMobile) return { Header: DrawerHeader, Title: DrawerTitle, Close: DrawerClose };
+    return { Header: SheetHeader, Title: SheetTitle, Close: SheetClose };
   }, [isMobile]);
 
   return (
-    <>
+    <div className="flex h-full flex-col">
       <Header>
         <div
-          className="cocktail-img aspect-[4/3]"
+          className={cn('cocktail-img aspect-[1/1]', { 'aspect-[9/5]': isMobile })}
           style={{
             backgroundImage: `url(${cocktail.image.url})`,
             backgroundRepeat: 'no-repeat',
@@ -30,11 +33,28 @@ export function CocktailDetails(props: CocktailDetailsProps) {
         />
         <Title className="text-left">{cocktail.name}</Title>
       </Header>
-      <div className="p-3">
+      <div className="flex flex-1 flex-col gap-1 p-3">
         {cocktail.recipe.map(item => (
-          <div key={item.ingredientId}>{item.ingredientId}</div>
+          <div key={item.ingredientId} className="ingredient flex flex-row">
+            <span>
+              {item.ingredient.name}
+              <span className="modifier text-slate-400">
+                {!!item.optional && ' (optional)'}
+                {!!item.garnish && ' (garnish)'}
+              </span>
+            </span>
+            {!item.garnish && (
+              <>
+                <span className="amount ml-auto">{item.amount}</span>
+                <span className="unit ml-1 text-slate-400">{item.unit.shortName}</span>
+              </>
+            )}
+          </div>
         ))}
+        <Button as={Close} className={cn('mt-auto w-full', { 'mt-10': isMobile })}>
+          Close
+        </Button>
       </div>
-    </>
+    </div>
   );
 }
